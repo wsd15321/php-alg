d4507f0ab9d46cd29e58eaa1ad4b268c9d1861e2
ihnnfnnbsa <3444157027@qq.com>
2018-06-20 16:39:10 +0800

    update

--- searching/BST.php
+++ searching/BST.php
@@ -7,25 +7,45 @@ namespace searching;
 class BST
 {
 
-    private $node = null;
-
-    /** @var BST */
-    private $left_node = null;
-
-    /** @var BST */
-    private $right_node = null;
-
-    /** @var int */
-    private $N = 0;
+    public $node = ['value' => null, 'left' => [], 'right' => [], 'N'=>0];
 
     /**
      * @param string|int $key
      * @param string|int|object|null $value
      */
-    public function put($value, $node = null)
+    public function put($value)
     {
-
+        $this->putValue($value, $this->node);
     }
+    
+    private function putValue($value, &$arr)
+    {
+        if ($arr['value'] === null) {
+            $arr['value'] = $value;
+        } elseif ($value > $arr['value']) {
+            if ($arr['right'] === []) {
+                $arr['right']['value'] = $value;
+                $arr['right']['right'] = [];
+                $arr['right']['left'] = [];
+                $arr['right']['N'] = $arr['N'] + 1;
+            } else {
+                $this->putValue($value, $arr['right']);
+            }
+        } elseif ($value < $arr['value']) {
+            if ($arr['left'] === []) {
+                $arr['left']['value'] = $value;
+                $arr['left']['right'] = [];
+                $arr['left']['left'] = [];
+                $arr['left']['N'] = $arr['N'] + 1;
+            } else {
+                $this->putValue($value, $arr['left']);
+            }
+        } else {
+            $arr['value'] = $value;
+            $arr['N'] = $arr['N'] + 1;
+        }
+    }
+
 
     /**
      * @param string|int $key
