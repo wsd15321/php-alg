db1ca4da1cf0c1e14a3bae49e808be3ce32bb076
ihnnfnnbsa <3444157027@qq.com>
2018-06-20 16:39:53 +0800

    update

--- test.php
+++ test.php
@@ -4,9 +4,13 @@ require_once(__DIR__ . '/Autoload.php');
 
 $st = new \searching\BST();
 
-$st->put(8);
-$st->put(5);
+$st->put(11);
+$st->put(10);
+$st->put(7);
+$st->put(17);
+$st->put(20);
+$st->put(2);
 $st->put(12);
-var_dump($st);
+var_dump($st->node);
 
 
